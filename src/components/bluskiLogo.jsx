87c6711801d58a37822745a72b91f727011f35ed
andrew hulme <andrew_hulme04@hotmail.com>
2024-07-05 18:04:14 +1000

    Added logo and navbar components

---
+++ src/components/bluskiLogo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function BluskiLogo() {
+  return(
+    <>
+      <div className="bluski-logo">
+        <img src="src/assets/images/BluskiLogo.jpg" alt="Bluski Plumbing Logo" />
+      </div>
+    </>
+  )
+}
+
+export default BluskiLogo;
