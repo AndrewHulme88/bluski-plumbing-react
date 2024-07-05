3b5a3ea44d16fd8c4c80546401b11b7a3f33b8b6
andrew hulme <andrew_hulme04@hotmail.com>
2024-07-05 18:34:55 +1000

    Imported bootstrap and implemented navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import BluskiLogo from './components/bluskiLogo'
+import MyNavbar from './components/myNavbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <MyNavbar />
       <BluskiLogo />
       <div>
         <a href="https://vitejs.dev" target="_blank">
