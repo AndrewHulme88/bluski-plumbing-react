773f5d8ad64847129f43190ee7f2c745cb7abd7b
andrew hulme <andrew_hulme04@hotmail.com>
2024-07-13 12:56:28 +1000

    Added details to credentials

--- src/components/credentials.jsx
+++ src/components/credentials.jsx
@@ -1,6 +1,12 @@
+import React from "react";
+
 function Credentials() {
   return (
-    <h1>Credentials</h1>
+    <>
+      <h1>Credentials</h1>
+      <p className="white">ABN - 59870348767</p>
+      <p className="white">VIC Plumbing Industry Commission - 117096</p>
+    </>
   );
 }
 
