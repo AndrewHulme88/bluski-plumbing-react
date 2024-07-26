1325adb3c5c78d254f7c7b9db9d846259d737ca8
andrew hulme <andrew_hulme04@hotmail.com>
2024-07-26 11:18:08 +1000

    Added sub images to carousel

--- src/components/showcase.jsx
+++ src/components/showcase.jsx
@@ -1,74 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import '../CarouselStyles.css'; // Import the CSS file
+
+const images = [
+  'src/assets/images/Screenshot 2024-03-04 171201.png',
+  'src/assets/images/Screenshot 2024-03-04 171216.png',
+  'src/assets/images/Screenshot 2024-03-04 171247.png',
+  'src/assets/images/Screenshot 2024-03-04 171331.png',
+  'src/assets/images/Screenshot 2024-03-04 171104.png',
+  'src/assets/images/Screenshot 2024-03-04 171026.png',
+  'src/assets/images/Screenshot 2024-03-04 170913.png',
+  'src/assets/images/Screenshot 2024-03-04 170956.png'
+];
 
 function Showcase() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleSelect = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
     <>
       <div className="showcase-heading">
         <h1>Our Work</h1>
       </div>
-      <Carousel>
-      <Carousel.Item>
-        <img src="src/assets/images/Screenshot 2024-03-04 171201.png" alt="" />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img src="src/assets/images/Screenshot 2024-03-04 171216.png" alt="" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img src="src/assets/images/Screenshot 2024-03-04 171247.png" alt="" />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img src="src/assets/images/Screenshot 2024-03-04 171331.png" alt="" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img src="src/assets/images/Screenshot 2024-03-04 171104.png" alt="" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img src="src/assets/images/Screenshot 2024-03-04 171026.png" alt="" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img src="src/assets/images/Screenshot 2024-03-04 170913.png" alt="" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img src="src/assets/images/Screenshot 2024-03-04 170956.png" alt="" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+      <Carousel activeIndex={activeIndex} onSelect={handleSelect}>
+        {images.map((img, index) => (
+          <Carousel.Item key={index}>
+            <img src={img} alt={`Image ${index + 1}`} />
+            <Carousel.Caption>
+              <h3>Image {index + 1} caption</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+      <div className="carousel-indicators">
+        {images.map((img, index) => (
+          <button
+            key={index}
+            type="button"
+            onClick={() => handleSelect(index)}
+            className={`carousel-indicator ${index === activeIndex ? 'active' : ''}`}
+          >
+            <img
+              src={img}
+              alt={`Image ${index + 1}`}
+              className="small-image"
+            />
+          </button>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
 export default Showcase;
